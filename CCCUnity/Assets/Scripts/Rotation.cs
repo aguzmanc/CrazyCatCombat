e1358d1b766738ac9982f87a5ac852306d2cb5b4
zeekkeen <zekken2307@gmail.com>
2019-04-22 15:34:51 -0400

    rotation

---
+++ CCCUnity/Assets/Scripts/Rotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [Range (0f,30f)]
+    public float speed = 10f;
+    
+
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
